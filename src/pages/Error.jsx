5cf59b7fbb61ab65ff96e95596d71afc3694f755
Rss631 <148336954+Rss631@users.noreply.github.com>
2023-11-04 19:53:51 +0530

    Add files via upload

---
+++ src/pages/Error.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Error = () => {
+  return (
+    <div className="justify-center items-center font-bold">
+      <h1>ERROR: PAGE NOT FOUND</h1>
+      <h3>Please go back to home page </h3>
+    </div>
+  );
+};
+
+export default Error;
